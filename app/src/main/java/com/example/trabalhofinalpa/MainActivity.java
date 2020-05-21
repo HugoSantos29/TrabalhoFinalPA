10ee000260124425548a7b5c0a5e36c8e611c79f
HugoSantos29 <hugofreches@gmail.com>
2020-05-21 11:18:26 +0100

    Validar que o nome tenha pelo menos um carater

--- app/src/main/java/com/example/trabalhofinalpa/MainActivity.java
+++ app/src/main/java/com/example/trabalhofinalpa/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.trabalhofinalpa;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +16,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void enviar(View view) {
+
+        EditText editTextNome = (EditText) findViewById(R.id.editTextNome);
+        String nome = editTextNome.getText().toString();
+
+        if (nome.length() == 0)
+
+    {
+        editTextNome.setError("Preencha o nome");
+        editTextNome.requestFocus();
+        return;
+    }
+
         Intent intent = new Intent(this, MostraDadosActivity.class);
 
         startActivity(intent);
