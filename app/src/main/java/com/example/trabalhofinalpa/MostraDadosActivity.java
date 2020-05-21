9259185c7df349d2428a017f85de87e08b4481c9
HugoSantos29 <hugofreches@gmail.com>
2020-05-21 10:16:49 +0100

    Adicionada atividade para mostrar os dados

---
+++ app/src/main/java/com/example/trabalhofinalpa/MostraDadosActivity.java
@@ -0,0 +1,14 @@
+package com.example.trabalhofinalpa;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MostraDadosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostra_dados);
+    }
+}
